05c4bdea5af636058a7c6339130831f0602f0d0a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-05-18 20:15:18 -0300

    Atualização

--- C/To-do/todo.c
+++ C/To-do/todo.c
@@ -2,50 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct tarefa {
-    char descricao[100];
+struct tarefa { // Estrutura para armazenar as tarefas
+    char descricao[100]; //Descrição da tarefa
 };
 
 void bordaMenu() {
-    printf("====================================\n");
+    printf("====================================\n"); //Borda do menu
 }
 
-int main() {
-    struct tarefa *lista = NULL;
-    int i, quantidade = 0, totalTarefas = 0, opcao;
+int main() { //Inicializador do programa
+    struct tarefa *lista = NULL; //Ponteiro para armazenar as posições das tarefas
+    int i, quantidade = 0, totalTarefas = 0, opcao; //Variaveis para uso do programa
 
-    do {
-        bordaMenu();
+    do {  //Inicio de um loop
+        bordaMenu(); 
         printf("Bem-vindo ao To-Do List\n");
         printf("1 -- Anotar tarefas\n");
         printf("2 -- Remover tarefas\n");
         printf("3 -- Mostrar tarefas\n");
-        printf("0 -- Sair\n");
+        printf("0 -- Sair\n"); //Opções do menui
         bordaMenu();
 
-        printf("Escolha uma opção: ");
+        printf("Escolha uma opção: "); 
         scanf("%d", &opcao);
-        getchar(); // limpar '\n'
+        getchar();  //Limpa o buffer de entrada
 
         switch (opcao) {
             case 1:
-                printf("Quantas tarefas deseja adicionar? ");
+                printf("Quantas tarefas deseja adicionar? "); 
                 scanf("%d", &quantidade);
-                getchar();
+                getchar(); 
 
-                lista = realloc(lista, (totalTarefas + quantidade) * sizeof(struct tarefa));
-                if (lista == NULL) {
+                lista = realloc(lista, (totalTarefas + quantidade) * sizeof(struct tarefa)); //Realoco espaço na memoria
+                if (lista == NULL) { //Verifica se o espaço foi alocado
                     printf("Erro ao alocar memória!\n");
-                    return 1;
+                    return 1; //Volta erro ao alocar a memoria
                 }
 
-                for (i = 0; i < quantidade; i++) {
-                    printf("Digite a tarefa %d: ", totalTarefas + 1);
-                    fgets(lista[totalTarefas].descricao, 100, stdin);
+                for (i = 0; i < quantidade; i++) { //Inicio de um loop
+                    printf("Digite a tarefa %d: ", totalTarefas + 1); 
+                    fgets(lista[totalTarefas].descricao, 100, stdin); 
                     lista[totalTarefas].descricao[strcspn(lista[totalTarefas].descricao, "\n")] = '\0';
-                    totalTarefas++;
+                    totalTarefas++; //Adiciona um numero para cada tarefa
                 }
-                break;
+                break; 
 
             case 2:
                 if (totalTarefas == 0) {
@@ -56,7 +56,7 @@ int main() {
                 printf("Tarefas atuais:\n");
                 for (i = 0; i < totalTarefas; i++) {
                     printf("%d. %s\n", i + 1, lista[i].descricao);
-                }
+                } //Mostra as tarefas atuais
 
                 int remover;
                 printf("Digite o número da tarefa que deseja remover: ");
@@ -67,10 +67,10 @@ int main() {
                     printf("Número inválido.\n");
                 } else {
                     // Move os elementos para "tapar o buraco"
-                    for (i = remover - 1; i < totalTarefas - 1; i++) {
-                        lista[i] = lista[i + 1];
+                    for (i = remover - 1; i < totalTarefas - 1; i++) { //i=remover - 1 (como começa o vetor do 0 subtrai 1 para achar a tarefa correta) , i < totalTarefas - 1: evita ultrapassar o fim do vetor (último índice válido é totalTarefas - 1).
+                        lista[i] = lista[i + 1]; //a tarefa atual (i) recebe o conteúdo da próxima (i + 1), mudando a posição na hora de imprimir as tarefas novamente.
                     }
-                    totalTarefas--;
+                    totalTarefas--; //Diminui o contador de tarefas
 
                     // Realoca para reduzir o tamanho
                     lista = realloc(lista, totalTarefas * sizeof(struct tarefa));
@@ -99,6 +99,6 @@ int main() {
 
     } while (opcao != 0);
 
-    free(lista);
+    free(lista); //Libera a memória que foi alocada dinamicamente com malloc(), calloc() ou realloc().
     return 0;
 }
