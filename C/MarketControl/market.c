3bd7f50552e17a6537ab2da1f012a8d09757da89
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-15 12:42:04 -0300

    Atualização

---
+++ C/MarketControl/market.c
@@ -0,0 +1,12 @@
+/*Cadastro de produtos (nome, código, preço, estoque)
+
+Registro de vendas (cliente, produtos comprados, total, data)
+
+Controle de estoque (baixa automática após venda)
+
+Relatório de produtos mais vendidos
+
+Relatório financeiro (total vendido por dia/mês)
+
+Carrinho de compra*/
+
